21c5984e9564aa04bf2a6b2b3af5a82c3e7b2762
lsalina-ai <lsalina@unal.edu.co>
2026-07-30 22:08:39 -0500

    Add files via upload

---
+++ ejercicio propuesto 4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Prueba {
+Vector profesores;
+public static void main(String[] args) {
+Prueba prueba = new Prueba();
+prueba.profesores = new Vector();
+Profesor profesor1 = new Profesor();
+ProfesorTitular profesor2 = new ProfesorTitular();
+prueba.profesores.add(profesor1);
+prueba.profesores.add(profesor2);
+for(int i = 0; i < prueba.profesores.size(); i++) {
+Profesor p = (Profesor) prueba.profesores.elementAt(i);
+p.imprimir();
+}
+}
+}
